465dbb92af95312a97934340f34549a84c606ff6
Bharath Kumar <bharathk494@gmail.com>
2020-04-24 20:39:09 +0530

    Initial Commit

---
+++ src/main/java/org/bharath/springframeworkdoubtclearance/SpringFrameworkDoubtClearanceApplication.java
@@ -0,0 +1,13 @@
+package org.bharath.springframeworkdoubtclearance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkDoubtClearanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkDoubtClearanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bharath/springframeworkdoubtclearance/SpringFrameworkDoubtClearanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bharath.springframeworkdoubtclearance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkDoubtClearanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
